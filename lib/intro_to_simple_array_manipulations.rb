513b6b90e5558259f5c80866996786ecd03f8f66
abstract-instructions-1268 <pranik.chainani@yale.edu>
2021-06-03 20:12:51 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -3,6 +3,22 @@ def using_concat(arr1, arr2)
 end
 
 def using_insert(arr, element)
-  arr.insert(element, 4)
+  arr.insert(4, element)
+end
+
+def using_uniq(arr)
+  arr.uniq
+end
+
+def using_flatten(arr)
+  arr.flatten
+end
+
+def using_delete(arr, string)
+  arr.delete(string)
+end
+
+def using_delete_at(arr, index)
+  arr.delete_at(index)
 end
 
